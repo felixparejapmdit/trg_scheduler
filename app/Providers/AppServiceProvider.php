18a84d4ab013e376bd988c2cb20a6925224a0ad5
felixparejapmdit <155054492+felixparejapmdit@users.noreply.github.com>
2024-07-02 10:57:37 +0800

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,7 +1,6 @@
 <?php
 
 namespace App\Providers;
-namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\View;
@@ -11,6 +10,7 @@ class AppServiceProvider extends ServiceProvider
 {
     public function boot()
     {
+        \Illuminate\Support\Facades\URL::forceScheme('https');
         View::composer('*', TitleComposer::class);
     }
 
